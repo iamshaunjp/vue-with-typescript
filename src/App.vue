7fc696d1eed60873fbc417a307f17b616893a702
Shaun <shaun@thenetninja.co.uk>
2021-04-23 16:14:27 +0100

    lesson-4

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <template>
   <div class="app">
-    <!-- <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('Zelda')">change name</button>
-    <button @click="changeAge('30')">change age</button> -->
-    
-    <p>{{ jobs[0].location }}</p>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from 'vue';
+import { defineComponent, ref } from 'vue';
 import Job from '@/types/Job'
+import JobList from './components/JobList.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {},
+  components: { JobList },
   setup() {
-    // const state = reactive({
-    //   name: 'Link',
-    //   age: 25 as number | string
-    // })
-  
-    // state.name = 999 // cannot change type
-    // state.age = '26' 
-
-    // return {...toRefs(state)}
-
-    // const name = ref('Link')
-    // const age = ref<number | string>(25)
-
-    // return { name, age }
-
     const jobs = ref<Job[]>([
       { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
       { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
@@ -40,16 +22,6 @@ export default defineComponent({
     ])
 
     return { jobs }
-  },
-  methods: {
-    // changeName(name: string) {
-    //   this.name = name
-    //   return name
-    // },
-    // changeAge(age: number | string) {
-    //   this.age = age
-    //   return age
-    // }
   }
 });
 </script>
---
+++ src/components/JobList.vue
@@ -0,0 +1,60 @@
+<template>
+  <ul class="job-list">
+    <li v-for="job in jobs" :key="job.id">
+      <h2>{{ job.title }} in {{ job.location }}</h2>
+      <div class="salary">
+        <p>{{ job.salary }} rupees</p>
+      </div>
+      <div class="description">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem omnis voluptatum eius doloremque optio iusto sequi dignissimos. Pariatur earum assumenda dolores possimus quidem quam, reprehenderit aliquid consequuntur amet non facere.</p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
+
+export default defineComponent({
+  props: {
+    jobs: {
+      type: Array as PropType<Job[]>,
+      required: true
+    }
+  },
+  setup(props) {
+    console.log(props.jobs)
+  },
+})
+</script>
+
+<style scoped>
+  .job-list {
+    padding: 0;
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
+</style>
