02ffa2e2f26cd03f9238b8ff4980cd65ff6f7900
Shaun <shaun@thenetninja.co.uk>
2021-04-23 16:02:55 +0100

    lesson-3

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
+    <!-- <p>{{ name }} - {{ age }}</p>
     <button @click="changeName('Zelda')">change name</button>
-    <button @click="changeAge('30')">change age</button>
+    <button @click="changeAge('30')">change age</button> -->
+    
+    <p>{{ jobs[0].location }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from '@/types/Job'
 
 export default defineComponent({
   name: 'App',
   components: {},
-  data() {
-    return {
-      name: 'Link',
-      age: 25 as number | string
-    }
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as number | string
+    // })
+  
+    // state.name = 999 // cannot change type
+    // state.age = '26' 
+
+    // return {...toRefs(state)}
+
+    // const name = ref('Link')
+    // const age = ref<number | string>(25)
+
+    // return { name, age }
+
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+
+    return { jobs }
   },
   methods: {
-    changeName(name: string) {
-      this.name = name
-      return name
-    },
-    changeAge(age: number | string) {
-      this.age = age
-      return age
-    }
+    // changeName(name: string) {
+    //   this.name = name
+    //   return name
+    // },
+    // changeAge(age: number | string) {
+    //   this.age = age
+    //   return age
+    // }
   }
 });
 </script>
---
+++ src/types/Job.ts
@@ -0,0 +1,8 @@
+interface Job {
+  title: string,
+  location: string,
+  salary: number,
+  id: string
+}
+
+export default Job
