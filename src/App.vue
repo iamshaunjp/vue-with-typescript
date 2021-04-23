6ef2b21322a15be80580a78ede9cfb2b57ec93ab
Shaun <shaun@thenetninja.co.uk>
2021-04-23 15:47:03 +0100

    lesson-2

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <!-- <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge('30')">change age</button> -->
+    <p>{{ jobs[0].location }}</p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, reactive, ref, toRefs } from 'vue';
+import Job from '@/types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    // const state = reactive({
+    //   name: 'Link',
+    //   age: 25 as number | string
+    // })
+  
+    // state.name = 999 // cannot change type
+    // state.age = '26' 
+
+    // return {...toRefs(state)}
+
+    // const name = ref('Link')
+    // const age = ref<number | string>(25)
+
+    // return { name, age }
+
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+
+    return { jobs }
+  },
+  methods: {
+    // changeName(name: string) {
+    //   this.name = name
+    //   return name
+    // },
+    // changeAge(age: number | string) {
+    //   this.age = age
+    //   return age
+    // }
   }
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
